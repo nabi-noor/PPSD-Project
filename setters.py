5e38e8d29192efa1a7e8e5efdb6b74ae9304447b
Wahab <abdulwahabpc52@gmail.com>
2019-12-15 19:24:06 +0500

    These are the setters

---
+++ setters.py
@@ -0,0 +1,11 @@
+ def setfirstname(self,first):
+        self.__firstName=first
+
+    def setlastname(self,last):
+        self.__lastName=last
+
+    def setEmail(self,mail):
+        self.__email=mail
+
+    def setphonenumber(self,num):
+        self.__phonenumber=num    
