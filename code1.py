30c7eb69a032f166153376a18f2867acaf5eabe5
Wahab <abdulwahabpc52@gmail.com>
2019-12-15 19:19:37 +0500

    this is constructors

---
+++ code1.py
@@ -0,0 +1,11 @@
+def __init__(self):
+        self.__firstName = ""
+        self.__lastName = ""
+        self.__phoneNumber = 0
+        self.__email = ""
+
+    def __init__(self,first,last,mail,num):
+        self.__firstName=first
+        self.__lastName=last
+        self.__email=mail
+        self.__phonenumber=num   
