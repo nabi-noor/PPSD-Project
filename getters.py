98642f28af9a190a5ba05fa68e1267581b13aae6
Wahab <abdulwahabpc52@gmail.com>
2019-12-15 19:26:00 +0500

    These are the getters

---
+++ getters.py
@@ -0,0 +1,11 @@
+ def getfirstname(self):
+        return self.__firstName
+
+    def getlastname(self):
+        return self.__lastName
+
+    def getemail(self):
+        return self.__email
+
+    def getphonenumber(self):
+        return self.__phonenumber      
